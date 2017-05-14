8f98f5ceb4df64bf7c0d4e2ada807b152d54d42a
Milan Sobat <Milan Sobat>
2017-05-13 20:37:12 -0400

    Added: ngx-tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,8 @@ import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 import { XLargeDirective } from './home/x-large';
+import { TooltipModule } from "ngx-tooltip";
+
 
 import '../styles/styles.scss';
 import '../styles/headings.css';
@@ -63,6 +65,7 @@ type StoreType = {
     FormsModule,
     HttpModule,
     Angular2ImageGalleryModule,
+    TooltipModule,
     AlertModule.forRoot(),
     CarouselModule.forRoot(),
     RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
